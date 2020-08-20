4b8adc01d5b56cb4b846ce97fe769dc7856dea3a
kakerberg <69698401+kakerberg@users.noreply.github.com>
2020-08-20 10:54:02 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Kevin Akerberg
+ *         kakerberg2@unl.edu
+ * Date: 2020/08/16
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Kevin Akerberg\n");
+
+  return 0;
+}
